961e9171e25510a81397a568b6c03ac472cc2b5b
Nikolai Shegai <nixer1978@yahoo.com>
2017-05-01 10:36:19 -0700

    Command line options (username, password) added

--- conftest.py
+++ conftest.py
@@ -9,12 +9,14 @@ def app(request):
     global fixture
     browser = request.config.getoption("--browser")
     base_url = request.config.getoption("--base_url")
+    username = request.config.getoption("--username")
+    password = request.config.getoption("--password")
     if fixture is None:
-         fixture = Application(browser=browser, base_url=base_url)
+         fixture = Application(browser=browser, base_url=base_url, username=username, password=password)
     else:
         if not fixture.is_valid():
             fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    fixture.session.ensure_login(username=username, password=password)
     return fixture
 
 
@@ -30,3 +32,6 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--base_url", action="store", default="http://17.218.71.53/addressbook/")
+    parser.addoption("--username", action="store", default="admin")
+    parser.addoption("--password", action="store", default="secret")
+
--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, base_url, username, password):
         if browser == "chrome":
             self.wd = webdriver.Chrome(ChromeDriverManager().install())
         elif browser == "firefox":
@@ -18,6 +18,8 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.base_url = base_url
+        self.username = username
+        self.password = password
 
     def is_valid(self):
         try:
