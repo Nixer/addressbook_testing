29882a9034e44ecdbd19be7155ac7c178a16e51b
Nikolai Shegai <nixer1978@yahoo.com>
2017-05-02 09:01:59 -0700

    Config file(json) added

--- conftest.py
+++ conftest.py
@@ -1,22 +1,22 @@
 import pytest
 from fixture.application import Application
+import json
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--base_url")
-    username = request.config.getoption("--username")
-    password = request.config.getoption("--password")
-    if fixture is None:
-         fixture = Application(browser=browser, base_url=base_url, username=username, password=password)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username=username, password=password)
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+         fixture = Application(browser=browser, base_url=target['base_url'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -31,6 +31,7 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--target", action="store", default="target.json")
     parser.addoption("--base_url", action="store", default="http://17.218.71.53/addressbook/")
     parser.addoption("--username", action="store", default="admin")
     parser.addoption("--password", action="store", default="secret")
